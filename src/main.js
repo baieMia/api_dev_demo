2151866b463cdae7259ac8eba6a5054f65d72bd9
zbMia <wm66400@163.com>
2019-03-19 14:56:08 +0800

    message

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+var origin='test';
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  data () {
+    return {
+      // 这里就是你的全局变量(属性)了，我这里三个属性值都是 ```function```,
+      // 当然你想写什么都行
+      origin:origin
+    }
+  }
+})
